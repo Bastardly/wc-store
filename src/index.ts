0f2dbca1ff082e604b576b5e3e4f3954cf82fe82
Flemming Knøss Hansen <fha@kmd.dk>
2025-03-24 09:40:02 +0100

    Bump to 1.0.1

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { define } from "./define";
 export { StoreElement } from "./store-element";
 export { Store } from "./store";
+export { StoreComposer } from "./store-composer";
 export { ObjectStore } from "./object-store";
