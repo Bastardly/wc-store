6342ac4ccc987ca37c35636fc5618fd38bc97900
Flemming Knøss Hansen (FHA) <fha@kmd.dk>
2025-12-19 14:38:55 +0100

    Add extended HTML element for easier development of web components

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { define } from "./define";
 export { StoreElement } from "./store-element";
 export { Store } from "./store";
 export { StoreComposer } from "./store-composer";
+export { ExtendedHTMLElement } from "./extended-html-element";
